b793ebb9b720b8e4214cee5033130c4fd320fa03
Mario Muscarella <mmuscar@illinois.edu>
2016-08-31 17:02:21 -0500

    adding code for branch lenght analysis

---
+++ bin/MothurTools.R
@@ -0,0 +1,60 @@
+################################################################################
+#                                                                              #
+# MothurTools Functions Source Code                                            #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Written by: Mario Muscarella                                                 #
+#                                                                              #
+# Last update: 2016/08/31                                                      #
+#                                                                              #
+################################################################################
+#                                                                              #
+# Notes: This code provides functions to be used in the analysis of            #
+#        16S rRNA sequence data post mothur anlaysis                           #
+#                                                                              #
+# Issues: Slow performance reading in OTU tables (common R issue)              #
+#                                                                              #
+# Recent Changes:                                                              #
+#                                                                              #
+# Future Changes (To-Do List):                                                 #
+#         1. Design functions to work with shared files in memory              #
+#         2. Add warnings                                                      #
+#                                                                              #
+################################################################################
+
+require("reshape")||install.packages("reshape");require("reshape")
+
+# Import OTU Site-by-Species Matrix
+read.otu <- function(shared = " ", cutoff = "0.03"){
+  matrix <- read.table(shared, header=T, fill=TRUE, comment.char="", sep="\t")
+  matrix.cutoff <- subset(matrix, matrix$label == cutoff)
+  matrix.out    <- as.matrix(matrix.cutoff[1:dim(matrix.cutoff)[1],
+                                           4:(3+mean(matrix.cutoff$numOtus))])
+  row.names(matrix.out) <- matrix.cutoff$Group
+  return(matrix.out)
+  }
+
+# Import Taxonomy Information
+read.tax <- function(taxonomy = " ", format = "rdp", col.tax = "3"){
+  tax_raw <- read.delim(taxonomy)                 # load genus-level data
+  if (format == "rdp"){
+    tax.info <- data.frame(matrix(NA, dim(tax_raw)[1], 6))
+    colnames(tax.info) <- c("Domain","Phylum","Class","Order","Family","Genus")
+    for (i in 1:dim(tax_raw)[1]){
+      tax.split <- strsplit(as.character(tax_raw[i,col.tax]), split="\\;")[[1]]
+      if (length(tax.split) < 6){
+        for (j in (length(tax.split)+1):6){
+             tax.split[j] = "unknown"
+        }}
+      tax.info[i,] <- tax.split
+    }
+    tax <- cbind(OTU = tax_raw[,1],tax.info)
+    for (i in 2:7){
+      tax[,i] <- gsub("\\(.*$", "", tax[,i])
+    }
+  } else {
+    stop("This funciton currently only works for RDP taxonomy")
+  }
+  return(tax)
+}
