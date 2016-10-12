8c1d7370ba130a8c5dd9a624d62a224e401e4559
Mario Muscarella <mmuscar@illinois.edu>
2016-10-12 10:55:11 -0500

    Working through Faprotax and adding to the microbial innovations figure
    Former-commit-id: 10a507a9222b6e464d33b5e59c5134a4a6d6fabb

---
+++ analyses/Traits.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm ../data/LTPs123.faprotax
+rm ../data/report.txt
+
+../bin/make_table.py ../data/LTPs123_unique.pds.wang.taxonomy ../data/LTPs123.OTUtable
+../bin/collapse_table.py -i ../data/LTPs123.OTUtable -g ../bin/FAPROTAX.txt -o ../data/LTPs123.faprotax --omit_columns 0 -d 'taxonomy' -c '#' -v -r ../data/report.txt
