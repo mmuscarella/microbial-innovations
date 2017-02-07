4c32309519e5b48c121466701a0e3c2b5589f7c9
Mario Muscarella <mmuscar@illinois.edu>
2017-02-07 00:41:27 -0600

    starting pure MC fitting functions
    Former-commit-id: 7d3f4be758682bdf8f008bf8655c9cfe9ad88ed3

--- analyses/TreeSimulation.R
+++ analyses/TreeSimulation.R
@@ -1,100 +1,6 @@
-# Define Functions
-sem <- function(x, ...){sd(x, na.rm = TRUE)/sqrt(length(na.omit(x)))}
+source("../bin/TreeSimulationFxns.R")
 
-TraitEvol <- function(birth = 0.2, a = 0.95, b = 0.98){
-  # Parameters
-  birth = birth; a = a; b = b
-  
-  # Generate Yule Tree
-  y.tree <-sim.bdtree(b = birth, d = 0, stop = "taxa", n = 100)
-  y.tree$tip.label <- paste("OTU", sprintf("%05d", seq(1:100)), sep = "")
-  
-  # Save a few values
-  Ntips <- length(y.tree$tip.label)
-  Nedges <- y.tree$Nnode + Ntips
-  
-  # Standardize Branch Lengths
-  tip.dist <- round(dist.nodes(y.tree)[1:Ntips, Ntips + 1], 5)
-  if (var(tip.dist) > 0.1){
-    stop("Tree may not be ultrametric")
-  }
-  std.fac <- mean(tip.dist)
-  y.tree$edge.length <- (y.tree$edge.length / std.fac) * 100
-  
-  # Generate Traits Matrix
-  traits <- matrix(NA, nrow = Nedges, ncol = 3)
-  colnames(traits) <- c("Parent", "Offspring", "Trait")
-  traits <- as.data.frame(traits)
-  
-  # Define Trait States
-  traitNames <- c("Off","On")
-  
-  # Define Root Ancestor Traits
-  traits[1,] <- c("None", "101", "Off")
-  
-  # Run Trait Model Given the Tree
-  for (i in 1:(Nedges - 1)){
-    traits[i + 1, 1:2] <- y.tree$edge[i, ]
-    t <- y.tree$edge.length[i]
-    p <- y.tree$edge[i,1]
-    o <- y.tree$edge[i,2]
-    index.p <- which(traits$Offspring == as.character(p))
-    init <- traits$Trait[index.p]
-    if (init == "Off"){u <- c(1,0)} else {
-      if (init == "On"){u <- c(0,1)}}
-    M <- MCtrait(t = t , a = a, b = b)
-    prob <- round(u %*% M$M, 4)
-    s <- sample(traitNames, size = 1, prob = prob)
-    traits[i + 1, 1] <- p   # Parent
-    traits[i + 1, 2] <- o   # Offspring
-    traits[i + 1, 3] <- s   # Trait State
-  }
-  
-  # Extract Node and Tip States
-  node.traits <- traits[which(as.numeric(traits$Offspring) > Ntips), ]
-  node.traits2 <- node.traits[order(as.numeric(node.traits$Offspring)), ]
-  tip.traits <- traits[which(as.numeric(traits$Offspring) <= Ntips), ]
-  tip.traits2 <- tip.traits[order(as.numeric(tip.traits$Offspring)), ]
-  
-  # Define Color Vectors
-  n.col <- node.traits2$Trait
-  n.col <- gsub("On", "red", gsub("Off", "gray", n.col))
-  t.col <- tip.traits2$Trait
-  t.col <- gsub("On", "red", gsub("Off", "gray", t.col))
-  
-  # Create Observed Traits Matrix
-  Obs.Traits <- data.frame(OTU = y.tree$tip.label, 
-                           Traits = tip.traits$Trait)
-  
-  # Add Parent Trait to Trait Matrix
-  traits$P.trait <- NA
-  traits$P.trait[1] <- "Off"
-  for (i in 2:dim(traits)[1]){
-    par.id <- which(traits$Offspring == traits$Parent[i])
-    traits$P.trait[i] <- traits$Trait[par.id]
-  }
-  
-  # Isolate Trait Evolution Events
-  trait.evol <- traits[which(traits$P.trait == "Off" & traits$Trait == "On"), ]
-  trait.evol$distance <- dist.nodes(y.tree)[trait.evol$Offspring, Ntips + 1]
-  
-  # Calculate Distance for 1st Evolution
-  min.evol <- min(trait.evol$distance)
-  
-  # Trait Evolution Plot
-  #plot(y.tree, "c", FALSE, no.margin = TRUE, label.offset = 4, cex = 0.3)
-  #mtext("Trait Simulation", side = 3, cex = 1, outer = F)
-  #nodelabels(node.traits2$Offspring, cex = 0.25, frame = "circle", bg = n.col)
-  #tiplabels(pch = 22, bg = t.col, adj = c(3, 0.5))
-  
-  return(min.evol)
-}
-
-TraitEvol.sim <- function(birth = 0.2, a = 0.95, b = 0.98, nsim = 100){
-  replicate(nsim, TraitEvol(birth, a, b))
-}
-
- 
+# Simulation 1: How does 1st Evolution Change with A
 test.a <- seq(0.55, 0.99, 0.01)
 test.b <- seq(0.55, 0.99, 0.01)
 
@@ -109,6 +15,7 @@ for (i in 1:dim(test1)[1]){
   test1$sim.sem[i] <- sem(sim)
 }
 
+# Simulation 2: How does 1st Evolution Change with B
 test2 <- data.frame(test.a = rep(0.90, length(test.b)), test.b = test.b,
                     sim.mean = rep(NA, length(test.b)),
                     sim.sem = rep(NA, length(test.b)))
@@ -120,8 +27,7 @@ for (i in 1:dim(test2)[1]){
   test2$sim.sem[i] <- sem(sim)
 }
 
-
-
+# Plot with Simulation 1 and 2
 layout(matrix(1:2, 1, 2))
 layout.show(2)
 
@@ -130,6 +36,8 @@ par(mar = c(3,3,1,1) + 0.1, oma = c(1, 1, 1, 1))
 plot(test1$test.a, test1$sim.mean)
 plot(test2$test.b, test2$sim.mean)
 
+
+# Test 3: 1st Evolution across ranges of A and B
 test.comb <- expand.grid(test.a, test.b)
 
 test3 <- data.frame(test.a = test.comb[,1], test.b = test.comb[,2],
@@ -142,6 +50,46 @@ for (i in 1:dim(test3)[1]){
   test3$sim.mean[i] <- mean(sim)
   test3$sim.sem[i] <- sem(sim)
 }
+
+# How does the likelihood of the root state change with A and B
+test.a <- seq(0.59, 0.99, 0.05)
+test.b <- seq(0.59, 0.99, 0.05)
+test.comb <- expand.grid(test.a, test.b)
+
+sim.lik <- data.frame(test.a = test.comb[,1], test.b = test.comb[,2],
+                               sim.mean = rep(NA, dim(test.comb)[1]),
+                               sim.sem = rep(NA, dim(test.comb)[1]))
+
+for (i in 1:dim(test.comb)[1]){
+  sim <- TraitEvol.sim2(birth = 0.2, a = test.comb[i, 1], b = test.comb[i, 2], 
+                       nsim = 10)
+  sim.lik$sim.mean[i] <- mean(sim[1, ])
+  #sim.lik$sim.sem[i] <- sem(sim[1, ])
+}
+x <- test.a
+y <- test.b
+z <- matrix(sim.lik[, 3], nrow = length(test.a), ncol = length(test.b))
+nrz <- nrow(z)
+ncz <- ncol(z)
+# Create a function interpolating colors in the range of specified colors
+jet.colors <- colorRampPalette( c("blue", "green") )
+# Generate the desired number of colors from this palette
+nbcol <- 100
+color <- jet.colors(nbcol)
+# Compute the z-value at the facet centres
+zfacet <- z[-1, -1] + z[-1, -ncz] + z[-nrz, -1] + z[-nrz, -ncz]
+# Recode facet z-values into color indices
+facetcol <- cut(zfacet, nbcol)
+par(mar = c(2, 2, 2, 2) + 0.5)
+persp(z = z, phi = 30, theta = 45,
+      zlim = c(0.25, 0.8), ticktype = "detailed", nticks = 4,
+      xlab = "Alpha", ylab = "Beta", zlab = "Likelihood of State",
+      col = color[facetcol], scale = FALSE)
+
+
+
+
+
 
 # Trait Predictions with ASR
 TraitASR <- function(tree = tree, traits = traits){
--- bin/TreeSimulationFxns.R
+++ bin/TreeSimulationFxns.R
@@ -192,19 +192,80 @@ TraitEvol2 <- function(birth = 0.2, a = 0.95, b = 0.98){
   min.evol <- min(trait.evol$distance)
 
 
-  return(list(y.tree, Obs.Traits, min.evol))
+  return(list(tree = y.tree, traits = Obs.Traits, min.evol = min.evol))
 }
 
 TraitEvol.sim.ASR <- function(birth = birth, a = a, b = b){
   temp <- TraitEvol2(birth, a, b)
   tree <- temp$tree
-  traits <- temp$Obs.Traits
-  ASR <- ace(traits, tree, type = "d", model = "ARD", CI = TRUE,
-         marginal = TRUE)
+  traits <- temp$traits
+  ASR <- ace(traits$Traits, tree, type = "d", model = "ARD", CI = TRUE,
+         marginal = TRUE, use.expm = TRUE)
   return(ASR)
 }
 
 # Calculate the Root State Likelihood
 TraitEvol.sim2 <- function(birth = 0.2, a = 0.95, b = 0.98, nsim = 100){
-  replicate(nsim, TraitEvol.sim.ASR(birth, a, b)$lik.anc)
+  replicate(nsim, TraitEvol.sim.ASR(birth, a, b)$lik.anc[1, ])
 }
+
+
+
+# Tree and Trait Simulation
+TT.sim <- function(birth = 0.2, a = 0.90, b = 0.90, tips = 100){
+  # Parameters
+  birth = birth; a = a; b = b; tips = tips
+  
+  # Generate Yule Tree
+  y.tree <-sim.bdtree(b = birth, d = 0, stop = "taxa", n = tips)
+  y.tree$tip.label <- paste("OTU", sprintf("%05d", seq(1:tips)), sep = "")
+  
+  # Save a few values
+  Ntips <- length(y.tree$tip.label)
+  Nedges <- y.tree$Nnode + Ntips
+  
+  # Standardize Branch Lengths
+  tip.dist <- round(dist.nodes(y.tree)[1:Ntips, Ntips + 1], 5)
+  if (var(tip.dist) > 0.1){
+    stop("Tree may not be ultrametric")
+  }
+  std.fac <- mean(tip.dist)
+  y.tree$edge.length <- (y.tree$edge.length / std.fac) * 100
+  
+  # Generate Traits Matrix
+  traits <- matrix(NA, nrow = Nedges, ncol = 3)
+  colnames(traits) <- c("Parent", "Offspring", "Trait")
+  traits <- as.data.frame(traits)
+  
+  # Define Trait States
+  traitNames <- c("Off","On")
+  
+  # Define Root Ancestor Traits
+  traits[1,] <- c("None", "101", "Off")
+  
+  # Run Trait Model Given the Tree
+  for (i in 1:(Nedges - 1)){
+    traits[i + 1, 1:2] <- y.tree$edge[i, ]
+    t <- y.tree$edge.length[i]
+    p <- y.tree$edge[i,1]
+    o <- y.tree$edge[i,2]
+    index.p <- which(traits$Offspring == as.character(p))
+    init <- traits$Trait[index.p]
+    if (init == "Off"){u <- c(1,0)} else {
+      if (init == "On"){u <- c(0,1)}}
+    M <- MCtrait(t = t , a = a, b = b)
+    prob <- round(u %*% M$M, 4)
+    s <- sample(traitNames, size = 1, prob = prob)
+    traits[i + 1, 1] <- p   # Parent
+    traits[i + 1, 2] <- o   # Offspring
+    traits[i + 1, 3] <- s   # Trait State
+  }
+  tree <- y.tree
+  trait <- traits$Trait[which(as.numeric(traits$Offspring) <= tips )]
+  names(trait) <- y.tree$tip.label
+  
+  return(list(tree = tree, traits = trait))
+
+}
+
+
