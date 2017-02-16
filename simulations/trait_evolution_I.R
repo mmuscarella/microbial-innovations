4d98c28199c77f3ca32c6d9ce5b5a992e86444d3
Mario Muscarella <mmuscar@illinois.edu>
2017-02-16 09:29:24 -0600

    adding print statements for error checking

--- simulations/trait_evolution_I.R
+++ simulations/trait_evolution_I.R
@@ -32,7 +32,7 @@ test1 <- data.frame(test.a = test.a, test.b = rep(0.90, length(test.a)),
                    sim.sem = rep(NA, length(test.a)))
 
 for (i in 1:dim(test1)[1]){
-  print(paste("Test 1:" i, "of", dim(test1)[1]))
+  print(paste("Test 1:", i, "of", dim(test1)[1]))
   sim <- TraitEvol.sim(birth = 0.2, a = test1$test.a[i], b = test1$test.b[i], 
                        nsim = 100)
   test1$sim.mean[i] <- mean(sim)
@@ -45,7 +45,7 @@ test2 <- data.frame(test.a = rep(0.90, length(test.b)), test.b = test.b,
                     sim.sem = rep(NA, length(test.b)))
 
 for (i in 1:dim(test2)[1]){
-  print(paste("Test 2:" i, "of", dim(test2)[1]))
+  print(paste("Test 2:", i, "of", dim(test2)[1]))
   sim <- TraitEvol.sim(birth = 0.2, a = test2$test.a[i], b = test2$test.b[i], 
                        nsim = 100)
   test2$sim.mean[i] <- mean(sim)
@@ -93,7 +93,7 @@ test3 <- data.frame(test.a = test.comb[,1], test.b = test.comb[,2],
                     sim.sem = rep(NA, dim(test.comb)[1]))
 
 for (i in 1:dim(test3)[1]){
-  print(paste("Test 3:" i, "of", dim(test3)[1]))
+  print(paste("Test 3:", i, "of", dim(test3)[1]))
   sim <- TraitEvol.sim(birth = 0.2, a = test3$test.a[i], b = test3$test.b[i], 
                        nsim = 100)
   test3$sim.mean[i] <- mean(sim)
