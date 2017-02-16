4989b0c4c0683624f635a6e5d810c67b9e3a2cab
Mario Muscarella <mmuscar@illinois.edu>
2017-02-15 19:07:54 -0600

    adding consenTrait analysis for IMG data, also moving analysis to script to run on Wilson
    Former-commit-id: b8ff1509989f6da52fc00a2efd2a79bfc140c9c4

---
+++ analyses/GG_ConsenTrait.R
@@ -0,0 +1,35 @@
+# Load Packages
+require("ape")
+require("phytools")
+require("picante")
+
+# Load Source Functions
+source("../bin/ConsenTrait.R")
+
+# Import Tree
+GG.um <- read.tree("../data/GG.dated.tree")
+
+
+# Import and Format Traits
+IMG.traits <- read.delim("../data/predicted_patyways.L3.txt", skip = 1, 
+                         row.names = 1)
+dim(IMG.traits)
+colnames(IMG.traits) <- gsub("X", "", colnames(IMG.traits))
+sum(GG.um$tip.label %in% colnames(IMG.traits))
+sum(colnames(IMG.traits) %in% GG.um$tip.label)
+rowSums(IMG.traits > 1)
+
+# Drop tips not found in trait table
+missing <- subset(GG.um$tip.label, 
+                  !(GG.um$tip.label %in% colnames(IMG.traits)))
+IMG.tree <- drop.tip(GreenTree.um, missing)
+
+# Reformat Trait Table
+IMG.trait.tab <- data.frame(colnames(IMG.traits), t(IMG.traits))
+
+# Run ConsenTrait
+trait.cons <- ConsenTrait(tree = IMG.tree, traits = IMG.trait.tab)
+
+# Save Output
+write.table(trait.cons, "../data/IMG_ConsenTrait.txt", quote = F,
+            sep = "\t", row.names = F, col.names = T)
