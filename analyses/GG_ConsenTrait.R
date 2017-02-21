9bbb60dd53fc13dea5bcfd2bed8f6e99b34c5fc4
Mario Muscarella <mmuscar@illinois.edu>
2017-02-20 18:18:26 -0600

    Making changes to ConsenTrait; Running on pathways and genes, fixing Node identification

--- analyses/GG_ConsenTrait.R
+++ analyses/GG_ConsenTrait.R
@@ -17,7 +17,7 @@ dim(IMG.traits)
 colnames(IMG.traits) <- gsub("X", "", colnames(IMG.traits))
 sum(GG.um$tip.label %in% colnames(IMG.traits))
 sum(colnames(IMG.traits) %in% GG.um$tip.label)
-rowSums(IMG.traits > 1)
+IMG.traits <- IMG.traits[rowSums(IMG.traits) > 10, ]
 
 # Drop tips not found in trait table
 missing <- subset(GG.um$tip.label, 
@@ -27,9 +27,41 @@ IMG.tree <- drop.tip(GG.um, missing)
 # Reformat Trait Table
 IMG.trait.tab <- data.frame(colnames(IMG.traits), t(IMG.traits))
 
-# Run ConsenTrait
+# Run ConsenTrait on Pathways
+print("Running ConsenTrait on Kegg Pathways")
 trait.cons <- ConsenTrait(tree = IMG.tree, traits = IMG.trait.tab)
-
+print("Completed ConsenTrait on Kegg Pathways")
 # Save Output
 write.table(trait.cons, "../data/IMG_ConsenTrait.txt", quote = F,
+            sep = "\t", row.names = F, col.names = T)
+
+
+# Import and Format Traits
+IMG.traits <- read.delim("../data/metagenome_predicted.txt", skip = 1, 
+                         row.names = 1)
+dim(IMG.traits)
+colnames(IMG.traits) <- gsub("X", "", colnames(IMG.traits))
+sum(GG.um$tip.label %in% colnames(IMG.traits))
+sum(colnames(IMG.traits) %in% GG.um$tip.label)
+IMG.traits <- IMG.traits[rowSums(IMG.traits) > 10, ]
+dim(IMG.traits)
+
+# Drop tips not found in trait table
+missing <- subset(GG.um$tip.label, 
+                  !(GG.um$tip.label %in% colnames(IMG.traits)))
+IMG.tree <- drop.tip(GG.um, missing)
+
+# Reformat Trait Table
+IMG.trait.tab <- data.frame(colnames(IMG.traits), t(IMG.traits))
+
+
+
+# Run ConsenTrait on Genes
+print("Running ConsenTrait on Kegg Genes")
+genetrait.cons <- ConsenTrait(tree = IMG.tree, traits = IMG.trait.tab)
+print("Completed ConsenTrait on Kegg Genes")
+
+
+# Save Output
+write.table(genetrait.cons, "../data/IMG_Genes_ConsenTrait.txt", quote = F,
             sep = "\t", row.names = F, col.names = T)
