8d534b943b88003c350709f755550fa3ce595509
Mario Muscarella <mmuscar@illinois.edu>
2017-04-10 14:24:21 -0500

    minor change, no plotting
    Former-commit-id: 2cf6d2a2c0716a7f09bbe2af51d8a6c4c3b592dd

--- simulations/joint_simulation_I.R
+++ simulations/joint_simulation_I.R
@@ -17,12 +17,12 @@ test.comb <- data.frame(expand.grid(X = test.x, Y = test.y))
 rep.comb <- data.frame(test.comb[rep(seq_len(nrow(test.comb)), each=10),])
 rownames(rep.comb) <- NULL
 
-mc.test <- matrix(NA, ncol = 4, nrow = dim(rep.comb)[1])
+mc.testA <- matrix(NA, ncol = 4, nrow = dim(rep.comb)[1])
 
 tree <- Tree.sim(tips = 2000, std = 4000)
 
-for (i in 1:dim(mc.test)[1]){
-  print(paste("Simulation", i, "out of", dim(mc.test)[1], sep = " "))
+for (i in 1:dim(mc.testA)[1]){
+  print(paste("Simulation", i, "out of", dim(mc.testA)[1], sep = " "))
   sim <- Trait.sim(tree = tree, x = rep.comb[i,1], y = rep.comb[i,2])
   tree <- sim$tree
   traits <- sim$traits
@@ -34,34 +34,34 @@ for (i in 1:dim(mc.test)[1]){
   
   # Isolate Output
   rates <- -ASR$rates
-  mc.test[i, 1:2] <- c(rates[1], rates[2])
-  #mc.test[i, 3:4] <- unlist(xy_to_ab(rates[1], rates[2]))
+  mc.testA[i, 1:2] <- c(rates[1], rates[2])
+  #mc.testA[i, 3:4] <- unlist(xy_to_ab(rates[1], rates[2]))
   } else {
-    mc.test[i, 1:2] <- c(NA, NA)
+    mc.testA[i, 1:2] <- c(NA, NA)
   }
 }
 
 # Plot Output
-png(filename="../simulations/output/Estimates.png",
-    width = 1800, height = 900, res = 96*2)
+#png(filename="../simulations/output/Estimates.png",
+#    width = 1800, height = 900, res = 96*2)
 
-layout(matrix(1:2, ncol = 2, byrow = T))
-par(mar= c(3,3,1,1))
-plot(y = log10(-mc.test[, 1]), x = log10(-rep.comb[, 1]),
-     xlab = "True X Rate (log10)", ylab = "Predicted X Rate (log10)")
+#layout(matrix(1:2, ncol = 2, byrow = T))
+#par(mar= c(3,3,1,1))
+#plot(y = log10(-mc.testA[, 1]), x = log10(-rep.comb[, 1]),
+#     xlab = "True X Rate (log10)", ylab = "Predicted X Rate (log10)")
 
-abline(0, 1)
+#abline(0, 1)
 
-plot(y = log10(-mc.test[, 2]), x = log10(-rep.comb[, 2]),
-     xlab = "True Y Rate (log10)", ylab = "Predicted Y Rate (log10)")
+#plot(y = log10(-mc.testA[, 2]), x = log10(-rep.comb[, 2]),
+#     xlab = "True Y Rate (log10)", ylab = "Predicted Y Rate (log10)")
 
-abline(0, 1)
+#abline(0, 1)
 
-dev.off()
-graphics.off()
+#dev.off()
+#graphics.off()
 
-img <- readPNG("../simulations/output/Estimates.png")
-grid.raster(img)
+#img <- readPNG("../simulations/output/Estimates.png")
+#grid.raster(img)
 
 
 
@@ -72,15 +72,15 @@ test.comb <- data.frame(expand.grid(X = test.x, Y = test.y))
 rep.comb <- data.frame(test.comb[rep(seq_len(nrow(test.comb)), each=10),])
 rownames(rep.comb) <- NULL
 
-mc.test <- data.frame(matrix(NA, ncol = 10, nrow = dim(rep.comb)[1]))
-colnames(mc.test) <- c("X", "Y", "X.p", "Y.p", 
+mc.testB <- data.frame(matrix(NA, ncol = 10, nrow = dim(rep.comb)[1]))
+colnames(mc.testB) <- c("X", "Y", "X.p", "Y.p", 
                        "ASR.Origins", "ASR.Mean", "ASR.SE",
                        "Con.Origins", "Con.Mean", "Con.SE")
 
 tree <- Tree.sim(tips = 2000, std = 4000)
 
-for (i in 1:dim(mc.test)[1]){
-  print(paste("Simulation", i, "out of", dim(mc.test)[1], sep = " "))
+for (i in 1:dim(mc.testB)[1]){
+  print(paste("Simulation", i, "out of", dim(mc.testB)[1], sep = " "))
   sim <- Trait.sim(tree = tree, x = rep.comb[i,1], y = rep.comb[i,2])
   tree <- sim$tree
   attributes(tree)$seed <- NULL
@@ -98,14 +98,14 @@ for (i in 1:dim(mc.test)[1]){
     
     # Isolate Output
     rates <- -ASR.r$rates
-    mc.test[i, 1:2] <- c(rep.comb[i,1], rep.comb[i,2])
-    mc.test[i, 3:4] <- c(rates[1], rates[2])
-    mc.test[i, 5] <- try(dim(ASR.o)[1])
-    mc.test[i, 6] <- try(mean(ASR.o$distance))
-    mc.test[i, 7] <- try(se(ASR.o$distance))
-    mc.test[i, 8] <- try(dim(CON)[1])
-    mc.test[i, 9] <- try(mean(CON$distance))
-    mc.test[i, 10] <- try(se(CON$distance))
+    mc.testB[i, 1:2] <- c(rep.comb[i,1], rep.comb[i,2])
+    mc.testB[i, 3:4] <- c(rates[1], rates[2])
+    mc.testB[i, 5] <- try(dim(ASR.o)[1])
+    mc.testB[i, 6] <- try(mean(ASR.o$distance))
+    mc.testB[i, 7] <- try(se(ASR.o$distance))
+    mc.testB[i, 8] <- try(dim(CON)[1])
+    mc.testB[i, 9] <- try(mean(CON$distance))
+    mc.testB[i, 10] <- try(se(CON$distance))
   }
 }
 
